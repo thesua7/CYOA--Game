3ac388fb92cf97230f5a28f0960ba1b3444ba077
Sani Ahamed <thesua711@gmail.com>
2020-05-14 02:54:51 +0600

    Canvas Modules Basics

---
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Text textComponent;
+    void Start()
+    {
+        textComponent.text = "I'm added programmatically!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
