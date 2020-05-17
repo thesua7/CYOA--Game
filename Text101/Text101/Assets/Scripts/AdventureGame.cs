8bd4693a9183caea03c6937ac6dd716f7e36b8a0
Sani Ahamed <thesua711@gmail.com>
2020-05-17 10:05:45 +0600

    All Done!
    UI Redesigned, Q=quit button now working

---
+++ Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+    
+    State state;
+    void Start()
+    {
+
+         state = StartingState;
+       
+         textComponent.text = state.GetStoryText();
+           
+        //textComponent.text = "I'm added programmatically!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+    
+    private void ManageState(){
+        var NextStates = state.GetNextState();
+
+         for(int i=0;i<NextStates.Length;i++){
+             if(Input.GetKeyDown(KeyCode.Alpha1+i)){
+
+                  state = NextStates[i];    
+                  
+             }
+
+             if(Input.GetKeyDown(KeyCode.Q)){
+                 state = StartingState;
+                 break;
+             }
+         }
+
+        
+         textComponent.text = state.GetStoryText();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     if(Input.GetKeyDown(KeyCode.Alpha1)){     //Key 1 = option 1
+    //         state = NextStates[0];
+    //     }
+
+    //     else if(Input.GetKeyDown(KeyCode.Alpha2)){ //Key 2 = option 2
+    //         state = NextStates[1];
+    //     }
+
+    //     else if(Input.GetKeyDown(KeyCode.Alpha3)){ //Key 3 = option 3
+    //         state = NextStates[2];
+    //     }
+
+    //     textComponent.text = state.GetStoryText();
+    // }
+
+    
+}
---
+++ Text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")] //Creating custom "State" option based on this class on menu
+public class State : ScriptableObject
+{
+
+   [TextArea(10,14)] [SerializeField] string StoryText;
+   [SerializeField] State[] nextState;
+    // Start is called before the first frame update
+
+    public string GetStoryText(){
+        return StoryText;
+    }
+
+    public State[] GetNextState(){
+        return nextState;
+    }
+
+}
